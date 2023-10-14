d774f6d26522ae0313ae0eb8c7455bef9d2bced9
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-10-13 19:28:13 -0700

    adding jsa website draft 1

---
+++ app/meta.tsx
@@ -0,0 +1,30 @@
+export const officers = [
+    {
+        name: 'Person 1',
+        position: 'President',
+        blurb: 'hello world',
+        image: '/images/placeholder.jpg',
+        socials: [
+            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+        ]
+    },
+    {
+        name: 'Person 2',
+        position: 'Co-Vice President',
+        blurb: 'hello world',
+        image: '/images/placeholder.jpg',
+        socials: [
+            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+        ]
+    },
+    {
+        name: 'Person 3',
+        position: 'Co-Vice President',
+        blurb: 'hello world',
+        image: '/images/placeholder.jpg',
+        socials: [
+            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+        ]
+    },
+]
+
