3b55b28e2fc930eef299b0b9c8ae43089195a85d
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-10-27 21:49:49 -0700

    new jsa info

--- app/meta.tsx
+++ app/meta.tsx
@@ -1,30 +1,42 @@
 export const officers = [
     {
-        name: 'Person 1',
-        position: 'President',
-        blurb: 'hello world',
-        image: '/images/placeholder.jpg',
+        name: 'Myles Kim',
+        position: 'Co-President',
+        blurb: 'Myles is a senior and co-president of Lynbrook JSA this year. He is an avid politics and history junkie who loves making fun of politicians. His policy areas of interest include urban planning and climate change. In his free time, he enjoys playing single-player story-driven video games and binge-watching his favorite Star Wars media.',
+        image: '/officers/myles_kim.png',
         socials: [
             { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+            { name: 'Email', link: 'mailto:mkim093@student.fuhsd.org'}
         ]
     },
     {
-        name: 'Person 2',
-        position: 'Co-Vice President',
-        blurb: 'hello world',
-        image: '/images/placeholder.jpg',
+        name: 'Kaawon Kim',
+        position: 'Co-President',
+        blurb: 'KK is a senior and co-president of Lynbrook JSA. She is part of Girls’ Golf, which she also plays recreationally, and Mock Trial. In her free time, she loves watching How I Met your Mother, baking, and playing with her two dogs. She is a true crime junkie who loves anything that makes toes curl. ',
+        image: '/officers/kaawon_kim.jpg',
         socials: [
             { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+            { name: 'Email', link: 'mailto:Kaawon14@gmail.com' }
         ]
     },
     {
-        name: 'Person 3',
-        position: 'Co-Vice President',
-        blurb: 'hello world',
+        name: 'Vikrant Vadathavoor ',
+        position: 'Example Position',
+        blurb: 'Example',
         image: '/images/placeholder.jpg',
         socials: [
             { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+            { name: 'Email', link: 'mailto:vvadathavoor052@student.fuhsd.org'}
+        ]
+    },
+    {
+        name: 'Chelsea Guo',
+        position: 'Vice President/PR Officer',
+        blurb: 'Chelsea is a sophomore and the vice president/PR officer of Lynbrook JSA this year. She is interested in politics and law as a member of the mock trial team as well. In her free time, she likes to write short stories, curl up with a good book, and take long walks. ',
+        image: '/officers/chelsea_guo.png',
+        socials: [
+            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
+            { name: 'Email', link: 'mailto:chgchz2020@gmail.com'}
         ]
     },
 ]
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,61 @@
+"use client"
+
 import Image from 'next/image'
 import { officers } from './meta'
-import { off } from 'process'
-
 
 
 export default function Home() {
   return (
-    <main className="w-screen bg-gray-100 flex min-h-screen flex-col items-center justify-center">
+    <main className="leading-relaxed p-28 w-screen bg-gray-100 flex min-h-screen flex-col items-center justify-center">
       <div className='gap-[2rem] flex flex-col'>
-        <h1 className='text-7xl font-extrabold text-red-900'>Lynbrook JSA</h1>
-        <div>
-          <h2 className='text-4xl font-bold text-blue-900'>What We Do</h2>
-          <p className='text-2xl font-[500]'>Summary: </p>
+        <h1 className='text-7xl font-extrabold text-red-900 leading-[3rem]'>Lynbrook JSA</h1>
+        <div className='leading-[3rem]'>
+          <h2 className='leading-[3rem] text-4xl font-bold text-blue-900'>What is Lynbrook JSA</h2>
+          <p className='leading-[3rem] text-2xl font-[500]'>
+            Junior State of America is a nationwide organization boasting over 500,000 Members and Alumni.  Founded in 1934, JSA was created by and for young people interested in politics, the government, and contributing to their community.
+          
+            Lynbrook Politics is a chapter of JSA and it is the only politics club on campus and serves as a nonpartisan forum for students to express their opinions.  Lynbrook Politics is, more specifically, a member of NorCal JSA.
+
+          </p>
         </div>
-        <h2 className='text-4xl font-bold text-blue-900'>Officers:</h2>
+        <div>
+          <h2 className='leading-[3rem] text-4xl font-bold text-blue-900 py-6'>Interested?</h2>
+          <button 
+            className='text-3xl font-semibold text-red-900 px-6 py-3 rounded-lg shadow-md' 
+            onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLSfkjhjHdKTgpWMvdlRVuObkZFNAdKEz6kSjpcZ5mPgMCLlVvA/viewform?usp=sf_link ")}
+          >Click Here!</button>
+        </div>
+        <h2 className='leading-[3rem] text-4xl font-bold text-blue-900'>Officers:</h2>
         <div>
           <div className='flex gap-3 flex-row'>
             {officers.map((officer) => (
-              <div className=' w-[20vw] h-[40vh] grid grid-rows-6 shadow-md rounded-[1rem] border-2'>
-                <div className='row-span-3'>
+              <div className=' w-[20vw] grid grid-rows-6 shadow-md rounded-[1rem] border-2'>
+                <div className='row-span-3 h-auto'>
                   <img 
                     src={officer.image} 
                     alt={officer.name} 
-                    className='aspect-auto'
+                    className='aspect-video h-[40vh]'
                   />
                 </div>
                 <div className='row-span-3 px-6 py-5 font-semibold'>
-                  <h2 className='text-3xl font-bold'>
+                  <h2 className='leading-[3rem] text-3xl font-bold'>
                     {officer.name}
                   </h2>
-                  <h2 className='text-blue-900 font-semibold text-[1.5rem]'>
+                  <h2 className='leading-[3rem] text-blue-900 font-semibold text-[1.5rem]'>
                     {officer.position}
                   </h2>
-                  <h2 className='text-2xl'>{officer.blurb}</h2>
+                  <h2 className='text-2xl leading-[3rem]'>{officer.blurb}</h2>
                   <div>
                     {officer.socials.map((social) => {
-                      let code = ""
-                      if(social.name == "Instagram") {code = "IG"}
-                      return (
-                        <>
+                      if(social.name == 'Email') {
+                        return (
+                          <>
                           <a href={social.link}>
-                            <div>{code}</div>
+                            <div>{social.name}</div>
                           </a>
                         </>
-                      )
+                        )
+                      }
                     })}
                   </div>
                 </div>
@@ -52,8 +64,8 @@ export default function Home() {
           </div>
         </div>
         <div className='space-y-[0.75rem]'>
-          <h2 className='text-4xl font-bold text-blue-900'>Get Involved</h2>
-          <h3 className='text-2xl font-[500]'>how to get involved</h3>
+          <h2 className='text-4xl font-bold text-blue-900 leading-[3rem]'>Get Involved</h2>
+          <h3 className='text-2xl font-[500] leading-[3rem]'>how to get involved</h3>
         </div>
       </div>
     </main>
