2584dea84e5fc5d5a429725858df2a0ae0692a24
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-10-28 14:21:42 -0700

    img_changes, more

--- app/meta.tsx
+++ app/meta.tsx
@@ -5,7 +5,6 @@ export const officers = [
         blurb: 'Myles is a senior and co-president of Lynbrook JSA this year. He is an avid politics and history junkie who loves making fun of politicians. His policy areas of interest include urban planning and climate change. In his free time, he enjoys playing single-player story-driven video games and binge-watching his favorite Star Wars media.',
         image: '/officers/myles_kim.png',
         socials: [
-            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
             { name: 'Email', link: 'mailto:mkim093@student.fuhsd.org'}
         ]
     },
@@ -15,7 +14,6 @@ export const officers = [
         blurb: 'KK is a senior and co-president of Lynbrook JSA. She is part of Girls’ Golf, which she also plays recreationally, and Mock Trial. In her free time, she loves watching How I Met your Mother, baking, and playing with her two dogs. She is a true crime junkie who loves anything that makes toes curl. ',
         image: '/officers/kaawon_kim.jpg',
         socials: [
-            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
             { name: 'Email', link: 'mailto:Kaawon14@gmail.com' }
         ]
     },
@@ -25,7 +23,6 @@ export const officers = [
         blurb: 'Example',
         image: '/images/placeholder.jpg',
         socials: [
-            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
             { name: 'Email', link: 'mailto:vvadathavoor052@student.fuhsd.org'}
         ]
     },
@@ -35,7 +32,6 @@ export const officers = [
         blurb: 'Chelsea is a sophomore and the vice president/PR officer of Lynbrook JSA this year. She is interested in politics and law as a member of the mock trial team as well. In her free time, she likes to write short stories, curl up with a good book, and take long walks. ',
         image: '/officers/chelsea_guo.png',
         socials: [
-            { name: 'Instagram', link: 'https://www.instagram.com/lynbrookjsa/' },
             { name: 'Email', link: 'mailto:chgchz2020@gmail.com'}
         ]
     },
--- app/page.tsx
+++ app/page.tsx
@@ -9,28 +9,26 @@ export default function Home() {
     <main className="leading-relaxed p-28 w-screen bg-gray-100 flex min-h-screen flex-col items-center justify-center">
       <div className='gap-[2rem] flex flex-col'>
         <h1 className='text-7xl font-extrabold text-red-900 leading-[3rem]'>Lynbrook JSA</h1>
-        <div className='leading-[3rem]'>
+        <div className='leading-[3rem] flex flex-col gap-3'>
           <h2 className='leading-[3rem] text-4xl font-bold text-blue-900'>What is Lynbrook JSA</h2>
           <p className='leading-[3rem] text-2xl font-[500]'>
-            Junior State of America is a nationwide organization boasting over 500,000 Members and Alumni.  Founded in 1934, JSA was created by and for young people interested in politics, the government, and contributing to their community.
-          
-            Lynbrook Politics is a chapter of JSA and it is the only politics club on campus and serves as a nonpartisan forum for students to express their opinions.  Lynbrook Politics is, more specifically, a member of NorCal JSA.
-
+            <span className='block py-[.5rem]'>Junior State of America is a nationwide organization boasting over 500,000 Members and Alumni.  Founded in 1934, JSA was created by and for young people interested in politics, the government, and contributing to their community.</span>
+            <span className='block py-[.5rem]'>Lynbrook Politics is a chapter of JSA and it is the only politics club on campus and serves as a nonpartisan forum for students to express their opinions.  Lynbrook Politics is, more specifically, a member of NorCal JSA.</span>
           </p>
         </div>
         <h2 className='leading-[3rem] text-4xl font-bold text-blue-900'>Officers:</h2>
         <div>
-          <div className='flex gap-3 flex-row'>
+          <div className='rounded-md w-full flex gap-[1rem] flex-row'>
             {officers.map((officer) => (
-              <div className=' w-[20vw] grid grid-rows-6 shadow-md rounded-[1rem] border-2'>
-                <div className='row-span-3 h-auto'>
+              <div className=' w-[20vw] shadow-md rounded-[1.5rem] border-2'>
+                <div className='h-auto'>
                   <img 
                     src={officer.image} 
                     alt={officer.name} 
-                    className='aspect-video h-[40vh]'
+                    className='rounded-t-[1.5rem] aspect-video object-cover h-[40vh]'
                   />
                 </div>
-                <div className='row-span-3 px-6 py-5 font-semibold'>
+                <div className='px-6 py-5 font-semibold'>
                   <h2 className='leading-[3rem] text-3xl font-bold'>
                     {officer.name}
                   </h2>
@@ -44,9 +42,17 @@ export default function Home() {
                         return (
                           <>
                           <a href={social.link}>
-                            <div>{social.name}</div>
+                            <div>Email</div>
                           </a>
                         </>
+                        )
+                      } else if (social.name = "Instagram") {
+                        return (
+                          <>
+                            <a href={social.link}>
+                              <div>IG</div>
+                            </a>
+                          </>
                         )
                       }
                     })}
@@ -59,7 +65,7 @@ export default function Home() {
         <div>
           <h2 className='leading-[3rem] text-4xl font-bold text-blue-900 py-6'>Interested?</h2>
           <button 
-            className='text-3xl font-semibold text-red-900 px-6 py-3 rounded-lg shadow-md' 
+            className='text-3xl font-semibold text-red-900 px-6 py-3 rounded-[0.75rem] shadow-md' 
             onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLSfkjhjHdKTgpWMvdlRVuObkZFNAdKEz6kSjpcZ5mPgMCLlVvA/viewform?usp=sf_link ")}
           >Click Here!</button>
         </div>
